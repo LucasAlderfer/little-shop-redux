d7ac73d101e97ed33ff6b9a3aad4e46cd54f45a3
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-06-26 17:45:29 -0600

    passing test for validation of merchant name

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 RSpec.describe Merchant do
   describe 'Validations' do
-    it 'is invalid without a name'
-    merchant = Merchant.new
+    it 'is invalid without a name' do
+      merchant = Merchant.new
 
-    expect(merchant).to_not be_valid
+      expect(merchant).to_not be_valid
+    end
   end
 end
 # 12334105,Shopin1901,2010-12-10,2011-12-04
