d1ad31e687624c579a09d99fe4fb0c17e100e184
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-06-26 17:57:30 -0600

    finished validations test for merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,9 +3,18 @@ require 'pry'
 RSpec.describe Merchant do
   describe 'Validations' do
     it 'is invalid without a name' do
-      merchant = Merchant.new
+      merchant = Merchant.new(id: 123, updated_at: 2012-12-10, created_at: 2012-11-10)
 
       expect(merchant).to_not be_valid
+    end
+    it 'is invalid without an id' do
+      merchant = Merchant.new(name: "Shopin1901", updated_at: 2012-12-10, created_at: 2012-11-10)
+    end
+    it 'is invalid without a created_at date' do
+      merchant = Merchant.new(id: 123, name: "Shopin1901", updated_at: 2012-12-10)
+    end
+    it 'is invalid without an updated_at date' do
+      merchant = Merchant.new(id: 123, name: "Shopin1901", created_at: 2012-11-10)
     end
   end
 end
