81f7c882a5844496544ca49d2bfed1a257db89e3
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-06-27 17:26:26 -0600

    Adds merchants page

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -5,17 +5,32 @@ class LittleShopApp < Sinatra::Base
     erb :'merchants/index'
   end
 
-  get '/merchants/:id' do
-    @merchant = Merchant.find_by_id(params[:id])
-    erb :'merchants/show'
-  end
-
-  get '/merchants/new' do
-    erb :'merchants/new'
-  end
-
-  post '/merchants' do
-    Merchant.create(params[:merchant])
-    redirect '/merchants'
-  end
+  # get '/merchants/:id' do
+  #   @merchant = Merchant.find_by_id(params[:id])
+  #   erb :'merchants/show'
+  # end
+  #
+  # get '/merchants/new' do
+  #   erb :'merchants/new'
+  # end
+  #
+  # post '/merchants' do
+  #   Merchant.create(params[:merchant])
+  #   redirect "/merchants/#{params[:merchant][:id]}"
+  # end
+  #
+  # get '/merchants/:id/edit' do
+  #   @merchant = Merchant.find(params[:id])
+  #   erb :'merchants/edit'
+  # end
+  #
+  # put '/merchants/:id' do
+  #   Merchant.update(id.to_i, params[:merchant])
+  #   redirect "/merchants/#{params[:id]}"
+  # end
+  #
+  # delete '/merchants/:id' do
+  #   Merchant.destroy(params[:id])
+  #   redirect '/merchants'
+  # end
 end
