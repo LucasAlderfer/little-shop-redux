01ff26e1f567714b0c181f509f8a8fd86a9b831e
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-06-27 16:04:34 -0600

    Seed functionality added

--- db/migrate/20180626225331_create_merchants.rb
+++ db/migrate/20180626225331_create_merchants.rb
@@ -2,7 +2,7 @@ class CreateMerchants < ActiveRecord::Migration[5.2]
   def change
     create_table :merchants, id: false do |t|
       t.integer :id
-      t.text :name
+      t.string :name
       t.datetime :created_at
       t.datetime :updated_at
     end
