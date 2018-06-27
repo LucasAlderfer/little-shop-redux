6df0cb7d7db655743589a178086f5888e11071a6
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-06-27 16:30:46 -0600

    Adds controller functionality

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,3 +1,21 @@
 class LittleShopApp < Sinatra::Base
-  
+
+  get '/merchants' do
+    @merchants = Merchant.all
+    erb :'merchants/index'
+  end
+
+  get '/merchants/:id' do
+    @merchant = Merchant.find_by_id(params[:id])
+    erb :'merchants/show'
+  end
+
+  get '/merchants/new' do
+    erb :'merchants/new'
+  end
+
+  post '/merchants' do
+    Merchant.create(params[:merchant])
+    redirect '/merchants'
+  end
 end
