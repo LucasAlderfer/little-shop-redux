12e80ded0cec5529d45cab4e9550acd5bda56fc7
JP Lynch <JPLynch35@gmail.com>
2018-06-28 14:52:54 -0600

    adds delete functionality

--- spec/features/user_see_merchant_index_spec.rb
+++ spec/features/user_see_merchant_index_spec.rb
@@ -12,10 +12,19 @@ RSpec.describe 'a visitor' do
       Merchant.create(id: 12345678, name:'Shopin1901', created_at: '2017-12-12', updated_at: '2018-02-05')
       visit '/merchants'
 
-      save_and_open_page
-
       click_link('edit')
       expect(current_path).to eq('/merchants/12345678/edit')
+    end
+    it 'should show a delete button that when clicked will delete the merchant' do
+      Merchant.create(id: 12345678, name:'Shopin1901', created_at: '2017-12-12', updated_at: '2018-02-05')
+      visit '/merchants'
+
+      save_and_open_page
+
+      click_link('delete')
+      within('.merchants_table') do
+        expect(page).to_not have_content('Shopin1901')
+      end
     end
   end
 end
--- textdump.rb
+++ textdump.rb
@@ -1,1 +1,4 @@
 <td><button href="/merchants/<%= merchant.id %>/edit" class="edit_button" id='test_edit_button'>edit</button></td>
+
+td><button class="delete_button">delete</button></td>
+
