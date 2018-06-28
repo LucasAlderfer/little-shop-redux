bce76db626d0e498dd9882ebe3ce32c5629b9d80
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-06-27 18:28:51 -0600

    Creates Index, New and Show views

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -5,19 +5,20 @@ class LittleShopApp < Sinatra::Base
     erb :'merchants/index'
   end
 
-  # get '/merchants/:id' do
-  #   @merchant = Merchant.find_by_id(params[:id])
-  #   erb :'merchants/show'
-  # end
-  #
-  # get '/merchants/new' do
-  #   erb :'merchants/new'
-  # end
-  #
-  # post '/merchants' do
-  #   Merchant.create(params[:merchant])
-  #   redirect "/merchants/#{params[:merchant][:id]}"
-  # end
+  get '/merchants/new' do
+    erb :'merchants/new'
+  end
+
+  post '/merchants' do
+    Merchant.create(params[:merchant])
+    redirect "/merchants/#{params[:merchant][:id]}"
+  end
+  
+  get '/merchants/:id' do
+    @merchant = Merchant.find_by_id(params[:id])
+    erb :'merchants/show'
+  end
+
   #
   # get '/merchants/:id/edit' do
   #   @merchant = Merchant.find(params[:id])
