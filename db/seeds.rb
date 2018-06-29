2d9e9b9f7dbb6e246ca89bd04648654d685cb75c
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-06-28 23:43:19 -0600

    WIP Set up Item DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
 require 'csv'
 require './app/models/merchant.rb'
+require './app/models/item.rb'
 
 Merchant.delete_all
+Item.delete_all
 
 CSV.foreach('./data/merchants.csv', headers: true, header_converters: :symbol) do |row|
   Merchant.create(id: row[0], name: row[1], created_at: row[2], updated_at: row[3])
 end
 
+CSV.foreach('./data/items.csv', headers: true, header_converters: :symbol) do |row|
+  Item.create(id: row[0], name: row[1], description: row[2], unit_price: row[3], merchant_id: row[4], created_at: row[5], updated_at: row[6])
+end
+
 puts Merchant.count
+puts Item.count
