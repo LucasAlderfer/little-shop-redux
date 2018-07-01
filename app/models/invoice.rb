b01971915dc5cf112a26400f40193b3a69a00110
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-06-30 19:21:56 -0600

    Adds tests for new invoice show features and instance method, and functionality

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,12 @@ class Invoice < ActiveRecord::Base
   belongs_to :merchant
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  
+
   validates_presence_of :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
 
+  def total_price
+    invoice_items.inject(0) do |sum, invoice_item|
+      sum += (invoice_item.quantity * invoice_item.item.unit_price)
+    end
+  end
 end
--- spec/features/user_can_see_invoice_items_on_invoice_show_page_spec.rb
+++ spec/features/user_can_see_invoice_items_on_invoice_show_page_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe 'a visitor' do
       item_2 = merchant.items.create(id: 12346, name: "AnotherItem", description: 'Better Item', unit_price: 12346, image_url: 'anything', created_at: '2018-06-27', updated_at: '2018-06-28')
       invoice_item_1 = invoice.invoice_items.create(id: 123456, item_id: 12345, quantity: 5, unit_price: 657, created_at: '2018-06-27', updated_at: '2018-06-28')
       invoice_item_2 = invoice.invoice_items.create(id: 123457, item_id: 12346, quantity: 7, unit_price: 650, created_at: '2018-06-27', updated_at: '2018-06-28')
-      require 'pry';binding.pry
 
       visit '/invoices/1050'
 
@@ -15,6 +14,23 @@ RSpec.describe 'a visitor' do
       expect(page).to have_content(item_2.name)
       expect(page).to have_content(invoice_item_1.quantity)
       expect(page).to have_content(invoice_item_2.quantity)
+      expect(page).to have_content(item_1.unit_price)
+      expect(page).to have_content(item_2.unit_price)
+      expect(page).to have_content(item_1.id)
+      expect(page).to have_content(item_2.id)
+    end
+    it 'should show the total price for the invoice' do
+      merchant = Merchant.create(id: 12348765, name:'Shopin1901', created_at: '2017-12-12', updated_at: '2018-02-05')
+      invoice = merchant.invoices.create(id: 1050, customer_id: 87654321, status: 'pending', updated_at: '2012-12-10', created_at: '2012-11-10')
+      item_1 = merchant.items.create(id: 12345, name: "AnItem", description: 'Good Item', unit_price: 12345, image_url: 'anything', created_at: '2018-06-27', updated_at: '2018-06-28')
+      item_2 = merchant.items.create(id: 12346, name: "AnotherItem", description: 'Better Item', unit_price: 12346, image_url: 'anything', created_at: '2018-06-27', updated_at: '2018-06-28')
+      invoice_item_1 = invoice.invoice_items.create(id: 123456, item_id: 12345, quantity: 5, unit_price: 657, created_at: '2018-06-27', updated_at: '2018-06-28')
+      invoice_item_2 = invoice.invoice_items.create(id: 123457, item_id: 12346, quantity: 7, unit_price: 650, created_at: '2018-06-27', updated_at: '2018-06-28')
+      expected_total = (invoice_item_1.quantity * item_1.unit_price) + (invoice_item_2.quantity * item_2.unit_price)
+
+      visit '/invoices/1050'
+
+      expect(page).to have_content(expected_total)
     end
   end
 end
