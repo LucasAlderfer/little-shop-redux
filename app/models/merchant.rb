b4cd09fa2753f327b20998b13f67c2fe2387d91f
JP Lynch <JPLynch35@gmail.com>
2018-07-01 13:05:35 -0600

    adds tests and functionality for iteration 10

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,25 @@ class Merchant < ActiveRecord::Base
 
   has_many :items
 
+  def total_items
+    items.count
+  end
+
+  def average_item_price
+    items.average(:unit_price)
+  end
+
+  def total_items_price
+    items.sum(:unit_price)
+  end
+
+  def self.most_items
+    all.max_by do |merchant|
+      merchant.total_items
+    end.name
+  end
+
+  def self.highest_priced_item
+    Item.where(unit_price: Item.maximum(:unit_price)).first.merchant.name
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -26,4 +26,64 @@ RSpec.describe Merchant do
       expect(merchant).to be_valid
     end
   end
+  describe 'Instance Methods' do
+    it 'can calculate total number of items for this merchant' do
+      merchant = Merchant.create(id: 123, name: "Shopin1901", updated_at: "2012-12-10", created_at: "2012-11-10")
+      item_1 = merchant.items.create(id: 1235, name: 'AnItem', description: 'Good Item', unit_price: 1000, image_url: 'anything', created_at: '2001-06-27', updated_at: '2002-06-28')
+      item_2 = merchant.items.create(id: 1236, name: 'AnotherItem', description: 'Good Item', unit_price: 2000, image_url: 'anything', created_at: '2018-06-27', updated_at: '2018-06-28')
+      item_3 = merchant.items.create(id: 1237, name: 'OtherItem', description: 'Good Item', unit_price: 3000, image_url: 'anything', created_at: '2018-06-30', updated_at: '2018-06-30')
+
+      expected = merchant.items.count
+
+      expect(merchant.total_items).to eq(expected)
+    end
+    it 'can calculate average price for all items from this merchant' do
+      merchant = Merchant.create(id: 123, name: "Shopin1901", updated_at: "2012-12-10", created_at: "2012-11-10")
+      item_1 = merchant.items.create(id: 1235, name: 'AnItem', description: 'Good Item', unit_price: 1000, image_url: 'anything', created_at: '2001-06-27', updated_at: '2002-06-28')
+      item_2 = merchant.items.create(id: 1236, name: 'AnotherItem', description: 'Good Item', unit_price: 2000, image_url: 'anything', created_at: '2018-06-27', updated_at: '2018-06-28')
+      item_3 = merchant.items.create(id: 1237, name: 'OtherItem', description: 'Good Item', unit_price: 3000, image_url: 'anything', created_at: '2018-06-30', updated_at: '2018-06-30')
+
+      expected = 2000
+
+      expect(merchant.average_item_price).to eq(expected)
+    end
+    it 'can calculate total price for all items from this merchant' do
+      merchant = Merchant.create(id: 123, name: "Shopin1901", updated_at: "2012-12-10", created_at: "2012-11-10")
+      item_1 = merchant.items.create(id: 1235, name: 'AnItem', description: 'Good Item', unit_price: 1000, image_url: 'anything', created_at: '2001-06-27', updated_at: '2002-06-28')
+      item_2 = merchant.items.create(id: 1236, name: 'AnotherItem', description: 'Good Item', unit_price: 2000, image_url: 'anything', created_at: '2018-06-27', updated_at: '2018-06-28')
+      item_3 = merchant.items.create(id: 1237, name: 'OtherItem', description: 'Good Item', unit_price: 3000, image_url: 'anything', created_at: '2018-06-30', updated_at: '2018-06-30')
+
+      expected = 6000
+
+      expect(merchant.total_items_price).to eq(expected)
+    end
+    describe 'Class Methods' do
+      it 'can calculate the merchant with the most items and output its name' do
+        merchant_1 = Merchant.create(id: 123, name: "Shopin1901", updated_at: "2012-12-10", created_at: "2012-11-10")
+        item_1a = merchant_1.items.create(id: 1235, name: 'AnItem', description: 'Good Item', unit_price: 1000, image_url: 'anything', created_at: '2001-06-27', updated_at: '2002-06-28')
+        item_2a = merchant_1.items.create(id: 1236, name: 'AnotherItem', description: 'Good Item', unit_price: 2000, image_url: 'anything', created_at: '2018-06-27', updated_at: '2018-06-28')
+        item_3a = merchant_1.items.create(id: 1237, name: 'OtherItem', description: 'Good Item', unit_price: 3000, image_url: 'anything', created_at: '2018-06-30', updated_at: '2018-06-30')
+
+        merchant_2 = Merchant.create(id: 125, name: "Shopin1905", updated_at: "2012-12-10", created_at: "2012-11-10")
+        item_2a = merchant_2.items.create(id: 1239, name: 'AnItem', description: 'Good Item', unit_price: 1000, image_url: 'anything', created_at: '2001-06-27', updated_at: '2002-06-28')
+  
+        expected = merchant_1.name
+  
+        expect(Merchant.most_items).to eq(expected)
+      end
+      it 'can calculate the merchant with highest price item and output its name' do
+        merchant_1 = Merchant.create(id: 123, name: "Shopin1901", updated_at: "2012-12-10", created_at: "2012-11-10")
+        item_1a = merchant_1.items.create(id: 1235, name: 'AnItem', description: 'Good Item', unit_price: 1000, image_url: 'anything', created_at: '2001-06-27', updated_at: '2002-06-28')
+        item_2a = merchant_1.items.create(id: 1236, name: 'AnotherItem', description: 'Good Item', unit_price: 2000, image_url: 'anything', created_at: '2018-06-27', updated_at: '2018-06-28')
+        item_3a = merchant_1.items.create(id: 1237, name: 'OtherItem', description: 'Good Item', unit_price: 3000, image_url: 'anything', created_at: '2018-06-30', updated_at: '2018-06-30')
+
+        merchant_2 = Merchant.create(id: 125, name: "Shopin1905", updated_at: "2012-12-10", created_at: "2012-11-10")
+        item_2a = merchant_2.items.create(id: 1239, name: 'AnItem', description: 'Good Item', unit_price: 9000, image_url: 'anything', created_at: '2001-06-27', updated_at: '2002-06-28')
+  
+        expected = merchant_2.name
+  
+        expect(Merchant.highest_priced_item).to eq(expected)
+      end
+    end
+  end
 end
