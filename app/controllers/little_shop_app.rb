56f08a9684906d7839c3db1fdfcb91124ebc15cc
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-01 21:08:14 -0600

    Fixes alignment on dashboard logo. Styles merchant dashboard

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -47,7 +47,7 @@ class LittleShopApp < Sinatra::Base
   end
 
   get '/merchants-dashboard' do
-    @merchants = Merchant.all
+    @merchants = Merchant.paginate(:page => params[:page], :per_page => 28)
     @most_items = Merchant.most_items
     @highest_price = Merchant.highest_priced_item
     erb :'merchants/dashboard'
